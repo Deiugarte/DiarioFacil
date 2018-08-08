3793f7fc8b9a7f3ef0518592640f65002feaf143
Diego <diego.alfaro6@ulatina.net>
2018-08-07 21:01:05 -0600

    Login

--- src/com/ulatina/diariofacil/DiarioFacil.java
+++ src/com/ulatina/diariofacil/DiarioFacil.java
@@ -7,6 +7,7 @@ package com.ulatina.diariofacil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -23,6 +24,66 @@ public class DiarioFacil {
 
     public DiarioFacil(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void inicio() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Bienvenido al sistema. Digite una opcion para continuar.");
+        System.out.println("1 - Login");
+        System.out.println("2 - Olvide mi contraseña");
+        System.out.println("3 - Registrarse");
+        System.out.println("4 - Salir");
+
+        switch (scan.nextInt()) {
+            case 1:
+                login();
+                break;
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Opcion no valida");
+                inicio();
+                break;
+        }
+
+    }
+
+    public void login() {
+        Scanner scan = new Scanner(System.in);
+        String cedula;
+        String pass;
+
+        System.out.println("Ingrese su cedula y contraseña o \"salir\" para regresar al menu principal");
+        System.out.print("Cedula: ");
+        cedula = scan.nextLine();
+
+        if (cedula.equals("salir") || cedula.equals("Salir") || cedula.equals("SALIR")) {
+            inicio();
+        }
+
+        System.out.print("Contraseña: ");
+        pass = scan.nextLine();
+
+        while (!verificarCredenciales(cedula, pass)) {
+            if (pass.equals("salir") || pass.equals("Salir") || pass.equals("SALIR")) {
+                inicio();
+            } else {
+                System.out.println("Contraseña o usuario invalido");
+            }
+            pass = scan.nextLine();
+        }
+        //TODO Go to "Landing Page"
+    }
+
+    private boolean verificarCredenciales(String cedula, String password) {
+        for (Usuario u : usuarios) {
+            if (u.getCedula().equals(cedula)) {
+                return u.getPassword().equals(password);
+            }
+        }
+        return false;
     }
 
     public String getNombre() {
---
+++ test/Tester.java
@@ -0,0 +1,24 @@
+
+import com.ulatina.diariofacil.DiarioFacil;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Diego
+ */
+public class Tester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DiarioFacil d = new DiarioFacil("Test");
+        d.inicio();
+    }
+    
+}
